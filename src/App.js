339267fb5ee81bb6fa7736c29534946a77cd3229
Esteban Santini <esantinie@gmail.com>
2017-07-05 15:47:49 -0700

    A simple Component that says hello to it's 'name' attribute.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<div className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h2>Welcome to React</h2>
+				</div>
+
+				<Welcome name="from App.js" />
+
+			</div>
+		);
+	}
+}
+
+class Welcome extends React.Component {
+	render() {
+		return <h1>Hello, {this.props.name}</h1>;
+	}
 }
 
 export default App;
