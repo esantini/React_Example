5ed3a039cb0ba4748de5eaeb974a65bbbe493f8e
Esteban Santini <esantinie@gmail.com>
2017-07-13 12:10:01 -0700

    Correction: gotta get "data" to get "cardsList"

--- src/myModules/kanban/kanban.tsx
+++ src/myModules/kanban/kanban.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import cardsList from './data.json';
+import data from './data.json';
 import List from './list';
 
 class kanban extends React.Component {
@@ -7,13 +7,13 @@ class kanban extends React.Component {
 		return (
 			<div className="kanban" style={{ border: '1px solid black', backgroundColor: '#eee', margin: '10px' }}>
 				<List id='todo' title="To Do" cards = {
- 						cardsList.filter( (card: kanban.Card) => card.status === "todo" )
+ 						data.cardsList.filter( (card: kanban.Card) => card.status === "todo" )
  					} />
  				<List id='in-progress' title="In Progress" cards = {
- 						cardsList.filter( (card: kanban.Card) => card.status === "in-progress" )
+ 						data.cardsList.filter( (card: kanban.Card) => card.status === "in-progress" )
  					} />
 				<List id='done' title='Done' cards = {
-						cardsList.filter( (card: kanban.Card) => card.status === "done" )
+						data.cardsList.filter( (card: kanban.Card) => card.status === "done" )
 					} />
 			</div>
 		);
