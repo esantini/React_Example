b2aad57d277fc9043a0065f98b73319b19babe33
Esteban Santini <esantinie@gmail.com>
2017-07-17 17:23:19 -0700

    Change CSS Class when Card's details are opened.

--- src/myModules/kanban/card.tsx
+++ src/myModules/kanban/card.tsx
@@ -5,23 +5,42 @@ class Card extends React.Component {
 
 	public props: kanban.Card;
 
+	public state: {
+		showDetails: boolean,
+	};
+
 	public constructor() {
 		super(...arguments);
 		this.state = {
-			showDetail: false,
+			showDetails: false,
 		};
 	}
 
 	public render() {
-		return (
-			<div className="card">
-				<div className="card__title">{this.props.title}</div>
+
+		let cardDetails;
+		if (this.state.showDetails) {
+			cardDetails = (
 				<div className="card__details">
 					{this.props.description}
 					<CheckList cardId={this.props.id} tasks={this.props.tasks} />
 				</div>
+			);
+		}
+
+		return (
+			<div className="card">
+				<div className={ this.state.showDetails ? "card__title card__title--is-open" : "card__title"}
+						onClick={ this.toggleDetails.bind(this) }>
+					{this.props.title}
+				</div>
+				{ cardDetails }
 			</div>
 		);
+	}
+
+	private toggleDetails() {
+		this.setState({ showDetails: !this.state.showDetails });
 	}
 }
 
