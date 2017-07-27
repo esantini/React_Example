6bf5e67b803e0229abe69b19ec8bed66ea2549c8
Esteban Santini <esantinie@gmail.com>
2017-07-27 11:36:13 -0700

    Basic Routing with "react-router-dom"

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
+
+import * as History from "history";
+
 import React, { Component } from "react";
+import { Link, Route, Router } from "react-router-dom";
+
 // import logo from './logo.svg'; // Use file-loader.
 // import './App.css';
 
@@ -14,84 +19,82 @@ import myKanbanData from "./myModules/kanban/data.json";
 interface IState {
 	route: string;
 }
+interface IProps {
+	children: any;
+}
 
 class App extends Component {
 
 	public state: IState;
+	public props: any;
 
-	public constructor() {
-		super(...arguments);
-		this.state = {
-			route: window.location.hash.substr(1),
-		};
-	}
-
-	public componentDidMount() {
-		window.addEventListener("hashchange", () => {
-			this.setState({
-				route: window.location.hash.substr(1),
-			});
-		});
-	}
+	// public componentWillReceiveProps(nextProps: any) {
+	// 	// will be true
+	// 	const locationChanged = nextProps.location !== this.props.location
+	// }
 
 	public render() {
-
-		let Child;
-		switch (this.state.route) {
-			case "/kanban": {
-				Child = <Kanban cards={ myKanbanData.cardsList } />;
-				break;
-			}
-			case "/list": Child = <ShoppingList />; break;
-			default: Child = <ShoppingCart />;
-		}
-
 		return (
-			<div className="App">
-				<div className="App-header">
-					{/*<img src={logo} className="App-logo" alt="logo" />*/}
-					<h2>Welcome to React</h2>
+			<Router history={History.createBrowserHistory()} >
+				<div className="App">
+					<div className="App-header">
+						{/*<img src={logo} className="App-logo" alt="logo" />*/}
+						<h2>Welcome to React</h2>
+
+						<Route path="/" exact={true} render={() => (
+							<h3>Home</h3>
+						)} />
+
+					</div>
+
+					<div className="routedStuff" >
+						<h3>The content of this div depends on the URL:</h3>
+
+						<menu>
+							<ul style={{margin: "0", padding: "0" }}>
+								<li><Link to="/">Home</Link></li>
+								<li><Link to="/shoppingCart">Shopping Cart</Link></li>
+								<li><Link to="/kanban">Kanban</Link></li>
+								<li><Link to="/list">Shopping List</Link></li>
+							</ul>
+						</menu>
+
+						<Route path="/shoppingCart" component={ShoppingCart} />
+						<Route path="/kanban" render={() => (
+							<h3 style={{ color: "green" }}>// TODO Kanban needs "Cards" property. </h3>
+						)} />
+						<Route path="/list" component={ShoppingList} />
+
+					</div>
+
+					<h3>Drag N Drop list</h3>
+					<ShoppingCart />
+
+					<hr />
+					<h3>Animated Shopping List:</h3>
+					<ShoppingList />
+
+					<hr />
+					<h3>Kanban with data from json file & pseudo CRUD functionality.</h3>
+					<Kanban cards={ myKanbanData.cardsList } />
+
+					<div style={{ border: "1px solid black", margin: "20px", backgroundColor: "beige" }}>
+						<h3>This gets the Kanban Data from a CORS requests.</h3>
+						<h4>CORS: Cross Origin Resource Sharing</h4>
+						<KanbanOnline />
+					</div>
+
+					<Welcome1 name="from App.tsx" />
+
+					<Clock />
+
+					<Toggle target="#light" />
+					<div>
+						<span id="light">Toggle turns me On/Off</span>
+					</div>
+
 				</div>
-
-				<div className="routedStuff" >
-					<h3>The content of this div depends on the URL:</h3>
-					<menu>
-						<ul style={{margin: "0", padding: "0" }}>
-							<li><a href="#/shoppingCart">Shopping Cart</a></li>
-							<li><a href="#/kanban">Kanban</a></li>
-							<li><a href="#/list">Shopping List</a></li>
-						</ul>
-					</menu>
-					{Child}
-				</div>
-
-				<h3>Drag N Drop list</h3>
-				<ShoppingCart />
-
-				<hr />
-				<h3>Animated Shopping List:</h3>
-				<ShoppingList />
-
-				<hr />
-				<h3>Kanban with data from json file & pseudo CRUD functionality.</h3>
-				<Kanban cards={ myKanbanData.cardsList } />
-
-				<div style={{ border: "1px solid black", margin: "20px", backgroundColor: "beige" }}>
-					<h3>This gets the Kanban Data from a CORS requests.</h3>
-					<h4>CORS: Cross Origin Resource Sharing</h4>
-					{/* <KanbanOnline /> */}
-				</div>
-
-				<Welcome1 name="from App.tsx" />
-
-				<Clock />
-
-				<Toggle target="#light" />
-				<div>
-					<span id="light">Toggle turns me On/Off</span>
-				</div>
-
-			</div>
+			</Router>
 		);
 	}
 }
