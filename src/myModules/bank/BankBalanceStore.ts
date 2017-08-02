cf485fffa21a72d2af343e58ba3dc7a559963b05
Esteban Santini <esantinie@gmail.com>
2017-08-02 12:45:04 -0700

    Store the balance as the state & register with the Dispatcher.

---
+++ src/myModules/bank/BankBalanceStore.ts
@@ -0,0 +1,43 @@
+import { EventEmitter, EventSubscription } from "fbemitter";
+import AppDispatcher from "../../AppDispatcher";
+import constants from "./constants";
+
+const CHANGE_EVENT = "change";
+const emitter = new EventEmitter();
+let balance = 0;
+
+interface IAction {
+	type: string;
+	ammount?: number;
+}
+
+const BankBalanceStore = {
+
+	getState() {
+		return balance;
+	},
+
+	addListener: (callback: (data?: any) => void) => {
+		return emitter.addListener(CHANGE_EVENT, callback);
+	},
+
+	dispatchToken: AppDispatcher.register((action: IAction) => {
+		switch (action.type) {
+			case constants.CREATED_ACCOUNT:
+				balance = 0;
+				emitter.emit(CHANGE_EVENT);
+				break;
+			case constants.DEPOSITED_INTO_ACCOUNT:
+				balance = balance + action.ammount!;
+				emitter.emit(CHANGE_EVENT);
+				break;
+			case constants.WITHDREW_FROM_ACCOUNT:
+				balance = balance - action.ammount!;
+				emitter.emit(CHANGE_EVENT);
+				break;
+		}
+	}),
+
+};
+
+export default BankBalanceStore;
