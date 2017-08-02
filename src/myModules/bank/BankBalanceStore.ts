d9504d005c5da0ca2d92da4f8524035edf1a9cec
Esteban Santini <esantinie@gmail.com>
2017-08-02 16:02:16 -0700

    Reduced to ReduceStore
    The balance is now the State wich is the number in ReduceStore<number, IAction>

--- src/myModules/bank/BankBalanceStore.ts
+++ src/myModules/bank/BankBalanceStore.ts
@@ -1,41 +1,39 @@
-import { Store } from "flux/utils";
+import { ReduceStore } from "flux/utils";
 
 import AppDispatcher from "../../AppDispatcher";
 import constants from "./constants";
-
-let balance = 0;
 
 interface IAction {
 	type: string;
 	ammount: number;
 }
 
-class BankBalanceStore extends Store<{}> {
+class BankBalanceStore extends ReduceStore<number, IAction> {
 
-	public getState() {
-		return balance;
+	public getInitialState() {
+		return 0;
 	}
 
-	// dispatchToken: AppDispatcher.register((action: IAction) => {
-	protected __onDispatch(action: IAction) {
+	public reduce(state: number, action: IAction) {
+
 		switch (action.type) {
+
 			case constants.CREATED_ACCOUNT:
-				balance = 0;
-				this.__emitChange();
-				break;
+				return 0;
+
 			case constants.DEPOSITED_INTO_ACCOUNT:
 				// TODO: Why is action.ammount NaN when I input nothing or a string?
 				if (Number.isNaN(action.ammount) ) { throw Error("Ammount is required"); }
-				balance = balance + action.ammount;
-				this.__emitChange();
-				break;
+				return state + action.ammount;
+
 			case constants.WITHDREW_FROM_ACCOUNT:
 				// TODO: Why is action.ammount NaN when I input nothing or a string?
 				if (Number.isNaN(action.ammount) ) { throw Error("Ammount is required"); }
-				balance = balance - action.ammount!;
-				this.__emitChange();
-				break;
+				return state - action.ammount;
+
 		}
+
+		return state;
 	}
 
 }
