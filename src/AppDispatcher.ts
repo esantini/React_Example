4fe7ba78b6c141c1987a72cb61e84acaab60a390
Esteban Santini <esantinie@gmail.com>
2017-08-11 15:24:54 -0700

    Async Dispatching

--- src/AppDispatcher.ts
+++ src/AppDispatcher.ts
@@ -10,6 +10,26 @@ class AppDispatcher extends Dispatcher<any> {
 		console.log("Dispatched", action);
 		super.dispatch(action);
 	}
+
+	/**
+	 * Dispatches three actions for an async operation represented by promise.
+	 */
+	public dispatchAsync(promise: any, types: any, payload?: any) {
+		console.log("Async Dispatch:", arguments);
+
+		const { request, success, failure } = types;
+		this.dispatch({ type: request, payload: Object.assign({}, payload) });
+		promise.then(
+			(response: any) => this.dispatch({
+				type: success,
+				payload: Object.assign({}, payload, { response }),
+			}),
+			(error: any) => this.dispatch({
+				type: failure,
+				payload: Object.assign({}, payload, { error }),
+			}),
+		);
+	}
 }
 
 export default new AppDispatcher();
