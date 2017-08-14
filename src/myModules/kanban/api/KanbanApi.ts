6c407a9fe78fc6f2559e5402bcaa6ff5863a0ea4
Esteban Santini <esantinie@gmail.com>
2017-08-13 22:48:38 -0700

    Kanban's CRUD fetch functions.

--- src/myModules/kanban/api/KanbanApi.ts
+++ src/myModules/kanban/api/KanbanApi.ts
@@ -16,6 +16,55 @@ const KanbanAPI = {
 				.then((response: any) => response.json() );
 	},
 
+	addCard(card: kanban.Card) {
+		return fetch(`${API_URL}/cards`, {
+			method: "post",
+			headers: API_HEADERS,
+			body: JSON.stringify(card),
+		})
+		.then( (response: Response) => response.json() );
+	},
+
+	updateCard( card: kanban.Card, draftCard: kanban.Card ) {
+		return fetch(`${API_URL}/cards/${card.id}`, {
+			method: "put",
+			headers: API_HEADERS,
+			body: JSON.stringify(draftCard),
+		});
+	},
+
+	persistCardDrag(cardId: number, status: string, index: number) {
+		return fetch(`${API_URL}/cards/${cardId}`, {
+			method: "put",
+			headers: API_HEADERS,
+			body: JSON.stringify({ status, row_order_position: index } ),
+		});
+	},
+
+	addTask( cardId: number, task: kanban.Task ) {
+		return fetch( `${API_URL}/cards/${cardId}/tasks`, {
+			method: "post",
+			headers: API_HEADERS,
+			body: JSON.stringify(task),
+		})
+		.then((response: Response) => response.json());
+	},
+
+	deleteTask( cardId: number, task: kanban.Task) {
+		return fetch(`${API_URL}/cards/${cardId}/tasks/${task.id}`, {
+			method: "delete",
+			headers: API_HEADERS,
+		});
+	},
+
+	toggleTask(cardId: number, task: kanban.Task ) {
+		return fetch(`${API_URL}/cards/${cardId}/tasks/${task.id}`, {
+			method: "put",
+			headers: API_HEADERS,
+			body: JSON.stringify({done: !task.done } ),
+		});
+	},
+
 };
 
 export default KanbanAPI;
